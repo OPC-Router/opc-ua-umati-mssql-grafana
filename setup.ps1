09331f119f3429cd5309476ad886b4434cc3d36e
fabiantautz <97020674+fabiantautz@users.noreply.github.com>
2022-01-05 15:09:41 +0100

    Updated installation

--- setup.ps1
+++ setup.ps1
@@ -25,6 +25,7 @@ $DockerInstaller = Join-Path $Env:Temp InstallDocker.msi
 echo ""
 echo "Welcome to the OPC Router 4 docker sample with Umati OPC UA, MSSQL and Grafana!"
 echo ""
+install_docker
 
 Try {
 	$test = docker --version
@@ -37,10 +38,10 @@ Catch
 {
 	install_docker
 }
-$GITHUB_ADRESS = "https://" + $GIT_TOKEN + "@github.com/OPC-Router/opc-ua-umati-mssql-grafana/archive/refs/heads/main.zip"
-Invoke-WebRequest -Uri $GITHUB_ADRESS -OutFile $TARGET_DIR
-$ExtractShell = New-Object -ComObject Shell.Application
-$ExtractFiles = $ExtractShell.Namespace($TARGET_DIR).Items()
+$GITHUB_ADRESS = "https://github.com/OPC-Router/opc-ua-umati-mssql-grafana/archive/refs/heads/main.zip"
+Invoke-WebRequest -Uri $GITHUB_ADRESS -OutFile "OPCRouter_Umati_MSSQL_Grafana.zip" -Headers @{'Authorization' = 'Basic dG9rZW46Z2hwX3hHbWJkc0pKS3FWRnhLaUVncWVLSWg5Mk4zQ0E3SDBVUmoyNg=='}
+Expand-Archive -Path "OPCRouter_Umati_MSSQL_Grafana.zip" -DestinationPath $TARGET_DIR -Force
+Remove-Item -Path "OPCRouter_Umati_MSSQL_Grafana.zip" -Force
 cd $TARGET_DIR
 sleep 10
 docker-compose up -d
